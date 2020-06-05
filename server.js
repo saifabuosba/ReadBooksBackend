ac25e07e70ea157e2915b0b52b3aac9a6327c4f6
saifabuosba <66364472+saifabuosba@users.noreply.github.com>
2020-06-05 15:35:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('consumption'); // Create a table for 'Book' with default settings
-
+mobileApp.tables.add('users'); 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
